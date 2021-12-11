d0cbd634498abcbde58b2cac42607e036424f22d
Draks238 <95897291+Draks238@users.noreply.github.com>
2021-12-10 20:56:51 -0500

    Add files via upload

---
+++ Ejecutar.bat
@@ -0,0 +1,5 @@
+-ECHO Presiona la tecla Enter para Iniciar el Servidor del Chat
+PAUSE
+cd C:\Chat E
+npm start
+PAUSE
---
+++ Instalar.bat
@@ -0,0 +1,5 @@
+-ECHO Presiona la tecla Enter para Instalar el Chat
+PAUSE
+cd C:\Chat E
+npm install
+
